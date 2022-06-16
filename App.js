299388a6fed049280c8beb25c61b7160610f2af3
benytto888Z <benamos111@gmail.com>
2022-06-16 02:03:58 +0200

    goalApp with scrollview

---
+++ App.js
@@ -0,0 +1,105 @@
+import { useState } from "react";
+import { StyleSheet, Text, View, Button, TextInput, Alert,ScrollView } from "react-native";
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { Feather,AntDesign } from '@expo/vector-icons'; 
+
+export default function App() {
+  const [newGoal, setNewGoal] = useState("");
+  const [courseGoals, setCourseGoals] = useState([]);
+  function goalinputHandler(enteredText) {
+    setNewGoal(enteredText);
+  }
+  function addGoalHandler() {
+    //setCourseGoals([...courseGoals,newGoal])
+    setCourseGoals((prevState) => [...prevState, newGoal]);
+  }
+
+  function editGoal(goalId){
+    Alert.alert(goalId);
+  }
+
+  return (
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Your course goal!!!"
+          onChangeText={goalinputHandler}
+        />
+        <Button title="Add Goal" onPress={addGoalHandler} />
+      </View>
+      <View style={styles.goalsContainer}>
+      <ScrollView alwaysBounceVertical={false}>
+        {courseGoals.map((goal,index) => {
+          return (
+            <View style={styles.goalContainer} key={index}>
+              <Text style={styles.textGoal}>{goal}</Text>
+              <View style={styles.actionBtns}>
+                  <Feather name="edit" size={24} color="black" onPress={()=>editGoal(goal)}/>
+                   <AntDesign name="delete" size={24} color="black" />
+               </View> 
+            
+            </View>
+          );
+        })}
+      </ScrollView>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  appContainer: {
+    flex: 1,
+    //flexDirection:'column',
+    paddingTop: 50,
+    paddingHorizontal: 16,
+    backgroundColor: "#CCEA8D",
+  },
+  inputContainer: {
+    flexDirection: "row",
+    flex: 1,
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor:'#A6BC09',
+    marginBottom: 24,
+    paddingHorizontal:20,
+    borderWidth: 3,
+    borderColor: "#005148",
+    borderRadius:9
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    width: "70%",
+    marginRight: 8,
+    padding: 8,
+    backgroundColor: "#fff",
+    textAlign: "center",
+    borderRadius: 8,
+  },
+
+  goalsContainer: {
+    flex: 5,
+  },
+  goalContainer: {
+    flexDirection:'row',
+    justifyContent: "space-between",
+    alignItems:'center',
+    padding:8,
+    marginBottom:6,
+    color:'white'
+  },
+  actionBtns:{
+   flexDirection:'row',
+
+  },
+  textGoal:{
+    color:'white', 
+    fontWeight:'bold',
+    backgroundColor:'#019587',
+    borderRadius:8,
+    padding:8,
+    width: '80%',
+  }
+});
